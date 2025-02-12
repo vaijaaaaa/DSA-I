c757380a3cbefa602ced0942ec45e844557334e8
vaijaaaaa <vaijuwalker111@gmail.com>
2025-02-12 22:05:03 +0530

    Largest odd in string

---
+++ String/Easy/LargestOdd.java
@@ -0,0 +1,15 @@
+package String.Easy;
+
+public class LargestOdd {
+    public String largestOddNumber(String num) {
+        int n = num.length();
+        for(int i = n-1; i>=0;i--){
+            char c = num.charAt(i);
+
+            if(c%2 == 1){
+                return num.substring(0,i+1);
+            }
+        }
+        return "";
+    }
+}
