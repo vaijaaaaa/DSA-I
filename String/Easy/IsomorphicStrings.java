28ec3781fba0bfe645172ed2eddfd8e0845954d8
vaijaaaaa <vaijuwalker111@gmail.com>
2025-02-16 22:00:16 +0530

    isomorphic string

---
+++ String/Easy/IsomorphicStrings.java
@@ -0,0 +1,19 @@
+package String.Easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character,Character> hm = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char ss = s.charAt(i);
+            char tt = t.charAt(i);
+            if((hm.containsKey(ss) && hm.get(ss) != tt || (!hm.containsKey(ss) && hm.values().contains(tt)))){
+                return false;
+            }
+            hm.put(ss,tt);
+        }
+        return true;
+    }
+}
