cd3a1c9f7e6173a5c4e925c4b4eda77838f843f8
vaijaaaaa <vaijuwalker111@gmail.com>
2025-02-19 20:23:59 +0530

    Serach in Linked List

---
+++ LinkedList/SearchInLinkedList.java
@@ -0,0 +1,23 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+public class SearchInLinkedList {
+    static boolean searchKey(int n, Node head, int key){
+        Node curr = head;
+
+        while(curr != null){
+            if(curr.data == key)
+                return true;
+            curr = curr.next;
+        }
+        return false;
+    }
+}
