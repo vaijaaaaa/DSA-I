9376d7ad55b1a5b9fd130b0c2aa4c0fbbebdcfa2
vaijaaaaa <vaijuwalker111@gmail.com>
2025-02-20 17:45:14 +0530

    Middle of the Linked List

---
+++ LinkedList/Easy/MiddleOfTheLinkedList.java
@@ -0,0 +1,25 @@
+class ListNode{
+    int data;
+    ListNode next;
+
+    ListNode(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+
+public class MiddleOfTheLinkedList {
+
+
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while( fast != null && fast.next != null ){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
