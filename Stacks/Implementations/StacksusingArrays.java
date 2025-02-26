bb4cbac058e2076a0f4da6e4e7df326b10a9be00
vaijaaaaa <vaijuwalker111@gmail.com>
2025-02-26 14:53:24 +0530

    stacks using arrays

---
+++ Stacks/Implementations/StacksusingArrays.java
@@ -0,0 +1,26 @@
+package Stacks.Implementations;
+
+public class StacksusingArrays {
+    class MyStack {
+        private int[] arr;
+        private int top;
+    
+        public MyStack() {
+            arr = new int[1000];
+            top = -1;
+        }
+    
+        public void push(int x) {
+            
+            arr[++top] = x;
+        }
+    
+        public int pop() {
+           if(top == -1){
+               return -1;
+           } 
+           return arr[top--];
+        }
+    }
+    
+}
