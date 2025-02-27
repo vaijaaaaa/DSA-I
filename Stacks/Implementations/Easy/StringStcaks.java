a4711659a59b7264eea39cec445d4742f02ef654
vaijaaaaa <vaijuwalker111@gmail.com>
2025-02-27 19:05:36 +0530

    string stacks

---
+++ Stacks/Implementations/Easy/StringStcaks.java
@@ -0,0 +1,17 @@
+public class StringStcaks {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i)== '['){
+                st.push(s.charAt(i));
+            }else{
+                if(st.isEmpty()) return false;
+                char ch = st.pop();
+                if((s.charAt(i) == ')' && ch == '(') || (s.charAt(i) == '}' && ch == '{')|| (s.charAt(i)==']' && ch == '[')) continue;
+                else return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
