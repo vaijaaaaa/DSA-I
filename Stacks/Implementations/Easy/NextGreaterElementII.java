06b28385c5efdb284b192599582c76610072d2bb
vaijaaaaa <vaijuwalker111@gmail.com>
2025-03-11 22:19:39 +0530

    Next Greater Element

---
+++ Stacks/Implementations/Easy/NextGreaterElementII.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class NextGreaterElementII {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        Stack<Integer> stack = new Stack<>();
+        int arr[] = new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i] = -1;
+        }
+
+        for(int i=0;i<n*2;i++){
+            while(!stack.isEmpty() && nums[stack.peek()] < nums[i%n]){
+                arr[stack.pop()] = nums[i%n];
+            }
+
+            if(i<n){
+                stack.push(i);
+            }
+        }
+        return arr;
+    }
+}
