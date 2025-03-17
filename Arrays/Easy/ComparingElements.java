59fd62fa7543cc92bf9d938e07b633a0fb17f38d
vaijaaaaa <vaijuwalker111@gmail.com>
2025-03-17 23:53:51 +0530

    Comparing Elements

--- Arrays/Easy/ComparingElements.java
+++ Arrays/Easy/ComparingElements.java
@@ -1,5 +1,24 @@
 public class ComparingElements {
+    static boolean compare(int arr[]){
+        int n = arr.length;
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i] == arr[j]){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
     public static void main(String[] args) {
-        
+        int arr[] = {10,20,30,40,50,10,60,70,80,90,100};
+        boolean ans = compare(arr);
+        if(ans){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
     }
 }
