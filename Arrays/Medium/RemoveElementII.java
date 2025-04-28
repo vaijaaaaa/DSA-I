603f17f653a83ab2c2a3e1d90103e6d5d79ecb9b
vaijaaaaa <vaijuwalker111@gmail.com>
2025-04-28 17:28:15 +0530

    Remove Element II

---
+++ Arrays/Medium/RemoveElementII.java
@@ -0,0 +1,14 @@
+package Arrays.Medium;
+
+public class RemoveElementII {
+    public int removeDuplicates(int[] nums) {
+        int k = 2;
+        for(int i=2;i<nums.length;i++){
+            if(nums[i]!=nums[k-2]){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
