30eaa16e142f9ef887e04916225255698564ce03
vaijaaaaa <vaijuwalker111@gmail.com>
2025-05-05 21:48:36 +0530

    sum maxmium subarray

--- Arrays/SubArrays/BasicSubArray.java
+++ Arrays/SubArrays/BasicSubArray.java
@@ -14,11 +14,25 @@ public class BasicSubArray {
         }
     }
 
-    
+    static void maximumsubarray(int arr[]){
+       int n = arr.length;
+       int max = Integer.MIN_VALUE;
+
+       for(int i=0;i<n;i++){
+        int sum = 0;
+        for(int j=i;j<n;j++){
+            sum += arr[j];
+            
+            max = Math.max(max,sum);
+        }
+       }
+       System.out.println(max);
+    }
 
     public static void main(String[] args) {
         int arr[] = {1,2,3,4};
         subarray(arr);
+        maximumsubarray(arr);
 
     }
 }
