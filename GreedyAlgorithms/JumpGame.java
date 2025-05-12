f31267219c8ca296c8d876284353bc30947c7e16
vaijaaaaa <vaijuwalker111@gmail.com>
2025-05-12 22:41:55 +0530

    Jump Game

---
+++ GreedyAlgorithms/JumpGame.java
@@ -0,0 +1,14 @@
+package GreedyAlgorithms;
+
+public class JumpGame {
+     public boolean canJump(int[] nums) {
+        int maxindex = 0;
+        for(int i=0;i<nums.length;i++){
+            if(i > maxindex){
+                return false;
+            }
+            maxindex = Math.max(maxindex,i+nums[i]);
+        }
+        return true;
+    }
+}
