5c11a723d84057bcd8ffacbc15032736b5f5ceb0
vaijaaaaa <vaijuwalker111@gmail.com>
2025-07-01 21:11:45 +0530

    fix

--- Arrays/SubArrays/BasicSubArray.java
+++ Arrays/SubArrays/BasicSubArray.java
@@ -7,7 +7,7 @@ public class BasicSubArray {
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
                 for(int k=i;k<=j;k++){
-                    System.out.print(arr[k] + " ");
+                    System.out.print(arr[k] + " "); 
                 }
                 System.out.println();
             }
