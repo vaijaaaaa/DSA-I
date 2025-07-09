2e3c91222812eaadb9a3c9807991fb07540666b7
vaijaaaaa <vaijuwalker111@gmail.com>
2025-07-09 10:50:42 +0530

    Refactor Fibonacci method to use recursion instead of iteration

--- BasicMath/Fibonacci.java
+++ BasicMath/Fibonacci.java
@@ -2,26 +2,20 @@ package BasicMath;
 
 public class Fibonacci {
 
-    // Method to find Fibonacci number using a for loop
-    public static int fibonacci(int n) {
-        if (n <= 1) {
-            return n;  // Return n if n is 0 or 1
+    public static int fibo(int n){
+        if(n==0){
+            return 0;
+        }else if(n == 1){
+            return 1;
         }
-
-        int a = 0, b = 1;  // Initializing the first two Fibonacci numbers
-
-        // Loop to calculate the nth Fibonacci number
-        for (int i = 2; i <= n; i++) {
-            int next = a + b;  // Calculate the next Fibonacci number
-            a = b;  // Move the previous number to 'a'
-            b = next;  // Move the next number to 'b'
-        }
-        
-        return b;  // Return the nth Fibonacci number
+        return fibo(n-1) + fibo(n-2);
     }
 
     public static void main(String[] args) {
-        int n = 10;  // Find the 10th Fibonacci number
-        System.out.println("Fibonacci of " + n + " is: " + fibonacci(n));
+        int count = 10;
+
+        for(int i=0;i<count;i++){
+            System.out.println(fibo(i));
+        }
     }
 }
