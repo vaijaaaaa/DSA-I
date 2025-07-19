d5415966426bbb38017f7627ab7b9f5c92a5967b
vaijaaaaa <vaijuwalker111@gmail.com>
2025-07-19 16:50:17 +0530

    Add SubarryBinary class to calculate the number of subarrays with a given sum

---
+++ GreedyAlgorithms/SubarryBinary.java
@@ -0,0 +1,20 @@
+package GreedyAlgorithms;
+
+public class SubarryBinary {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int n = nums.length;
+        int count = 0;
+
+
+        for(int i=0;i<n;i++){
+            int sum = 0;
+            for(int j=i;j<n;j++){
+                sum += nums[j];
+                if(sum == goal){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
