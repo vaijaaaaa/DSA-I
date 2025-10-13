d6a700efd53b4fa1673d4a61e19d786335487331
vaijaaaaa <vaijuwalker111@gmail.com>
2025-10-13 12:46:44 +0530

    572. Subtree of Another Tree

---
+++ BinaryTrees/SubtreeofAnotherTree.java
@@ -0,0 +1,27 @@
+public class SubtreeofAnotherTree {
+     public String postorder(TreeNode node){
+        if(node == null){
+            return "null";
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(node.val);
+        sb.append(postorder(node.left));
+        sb.append(postorder(node.right));
+
+        return sb.toString();
+    }
+
+
+
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        
+        String fulltree = postorder(root);
+        String subtree = postorder(subRoot);
+
+        if(!fulltree.contains(subtree)){
+            return false;
+        }
+        return true;
+    }
+}
