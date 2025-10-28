99b14497b3d10c3906153676d1bdb512dc162ba3
vaijaaaaa <vaijuwalker111@gmail.com>
2025-10-28 22:26:51 +0530

    42. Trapping Rain Water

---
+++ Arrays/Hard/TrappingRainWater.java
@@ -0,0 +1,30 @@
+package Arrays.Hard;
+
+public class TrappingRainWater {
+    public int trap(int[] height) {
+        int n = height.length;
+        int leftmax = 0;
+        int rightmax = 0;
+        int l = 0;
+        int r = n-1;
+        int ans = 0;
+
+        while(l<r){
+            if(height[l] > leftmax){
+                leftmax = Math.max(leftmax,height[l]);
+            }
+            if(height[r] > rightmax){
+                rightmax = Math.max(rightmax,height[r]);
+            }
+
+            if(leftmax < rightmax){
+                ans += leftmax - height[l];
+                l++;
+            }else{
+                ans += rightmax - height[r];
+                r--;
+            }
+        }
+        return ans;
+    }
+}
