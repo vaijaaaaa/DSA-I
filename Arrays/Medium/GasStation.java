468026de8280002086e2c295a1049ebb9f064c24
vaijaaaaa <vaijuwalker111@gmail.com>
2025-10-28 20:53:54 +0530

    134. Gas Station

---
+++ Arrays/Medium/GasStation.java
@@ -0,0 +1,22 @@
+package Arrays.Medium;
+
+public class GasStation {
+     public int canCompleteCircuit(int[] gas, int[] cost) {
+        int pos = 0;
+        int sum = 0;
+        int total = 0;
+
+        for(int i=0;i<gas.length;i++){
+            sum += gas[i] - cost[i];
+
+            if(sum < 0){
+                total += sum;
+                sum = 0;
+                pos = i + 1;
+            }
+        }   
+        total += sum;
+
+        return total >= 0 ? pos : -1;
+    }
+}
