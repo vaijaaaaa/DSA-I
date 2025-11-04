98f24caff1526ad190eb74c6bab615fed8f901c9
vaijaaaaa <vaijuwalker111@gmail.com>
2025-11-04 17:26:17 +0530

    547. Number of Provinces

---
+++ Graphs/NumberOfProvinces.java
@@ -0,0 +1,28 @@
+package Graphs;
+
+public class NumberOfProvinces {
+    public int findCircleNum(int[][] isConnected) {
+
+        int n = isConnected.length;
+        boolean visited[] = new boolean[n];
+        int count = 0;
+
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                dfs(isConnected,visited,i);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    void dfs(int[][] isConnected, boolean visited[], int i){
+        visited[i] = true;
+
+        for(int j = 0; j < isConnected.length;j++){
+            if(isConnected[i][j] == 1 && !visited[j]){
+                dfs(isConnected,visited,j);
+            }
+        }
+    }
+}
