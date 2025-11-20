65865306f0ba8301d01b82f3b52985cc3c9d48d7
vaijaaaaa <vaijuwalker111@gmail.com>
2025-11-20 14:39:08 +0530

    2486. Append Characters to String to Make Subsequence

---
+++ TwoPointers/AppendCharacterstoStringtoMakeSubsequence.java
@@ -0,0 +1,19 @@
+package TwoPointers;
+
+public class AppendCharacterstoStringtoMakeSubsequence {
+    public int appendCharacters(String s, String t) {
+        int i = 0;
+        int j = 0;
+
+        while(i < s.length() && j < t.length()){
+            if(s.charAt(i) == t.charAt(j)){
+                i++;
+                j++;
+            }else{
+                i++;
+    
+            }
+        }
+        return t.length() - j;
+    }
+}
