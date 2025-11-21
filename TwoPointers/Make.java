8888d318329a8815834dfc9f4dd756d79bc73bd5
vaijaaaaa <vaijuwalker111@gmail.com>
2025-11-21 15:02:24 +0530

    2825. Make String a Subsequence Using Cyclic Increments

---
+++ TwoPointers/Make.java
@@ -0,0 +1,19 @@
+package TwoPointers;
+
+public class Make String a SubsequenceUsingCyclicIncrements {
+     public boolean canMakeSubsequence(String str1, String str2) {
+        int i = 0, j = 0;
+
+        while (i < str1.length() && j < str2.length()) {
+            char c1 = str1.charAt(i);
+            char c2 = str2.charAt(j);
+
+       
+            if (c1 == c2 || (c1 + 1 == c2) || (c1 == 'z' && c2 == 'a')) {
+                j++;
+            }
+            i++;
+        }
+        return j == str2.length();
+    }
+}
